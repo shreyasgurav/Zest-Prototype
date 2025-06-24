4b73c4499440937000e09eb29df6971585b9dc5a
Shreyas Gurav <shrreyasgurav@gmail.com>
2025-06-24 13:42:14 +0530

    fixing issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import GuidesSection from '@/components/GuidesSection/GuidesSection';
 import EventsSection from '@/components/EventsSection/EventsSection';
 import ActivitySection from '@/components/ActivitySection/ActivitySection';
 
